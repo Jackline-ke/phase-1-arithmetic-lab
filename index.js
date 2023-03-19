c78b3f708cfea74b66441daee429947feab74dc4
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-19 11:53:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 =31;
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()*10)
+
+let num3 = 10;
+let num4 = 6;
+let mod =(num3 % num4);
+
+
+let max = Math.max(2,4,6,8,10,12,14,16,18,20);
+
+console.log(number);
+
